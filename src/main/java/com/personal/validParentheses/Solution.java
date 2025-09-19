9467601f01133bcde64edfeac0c449f2b377c668
kurromiii <ahadzadeparnian@gmail.com>
2025-09-19 22:58:22 +0330

    20. Valid Parentheses

---
+++ src/main/java/com/personal/validParentheses/Solution.java
@@ -0,0 +1,47 @@
+package com.personal.validParentheses;
+
+import java.util.Stack;
+
+//"()"
+//"()[]{}"
+//"(]"
+//"([])"
+//"([)]"
+
+//"(])"
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+        System.out.println(solution.isValid("(])"));
+    }
+
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            }
+            else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                if (stack.peek() == '(' && s.charAt(i) == ')' ||
+                        stack.peek() == '{' && s.charAt(i) == '}' ||
+                        stack.peek() == '[' && s.charAt(i) == ']') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
