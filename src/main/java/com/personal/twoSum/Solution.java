6346b931d9078b59f940a9e98cbdf83e54139208
kurromiii <ahadzadeparnian@gmail.com>
2025-09-19 22:24:44 +0330

    1. Two Sum - o(n^2) solution needs to be better

---
+++ src/main/java/com/personal/twoSum/Solution.java
@@ -0,0 +1,30 @@
+package com.personal.twoSum;
+
+import java.util.Arrays;
+
+//[2,7,11,15] target= 9
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+
+        int target = 9;
+
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(solution.twoSum(nums, target)));
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+
+        //o(n^2) this is bad
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+
+    }
+}
